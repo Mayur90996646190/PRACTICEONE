e45b4e9868bc6d52efe4fa4dfe241a4cf4464107
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-KEFG07B>
2023-09-27 20:47:00 +0530

    Child Commit 1

---
+++ src/test/java/TEST/CreateOrganizationTest.java
@@ -0,0 +1,12 @@
+package TEST;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganizationTest {
+
+   @Test
+   public void createOrganizationTest1()
+   {
+	   System.out.println("From Create Organization");
+   }
+}
